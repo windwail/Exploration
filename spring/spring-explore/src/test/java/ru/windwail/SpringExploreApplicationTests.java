2c34af8207d1309248b8abbf941ca0c1130cdb17
windwail <windwail@mail.ru>
2016-07-22 15:20:26 +0300

    new spring-explore project

---
+++ spring/spring-explore/src/test/java/ru/windwail/SpringExploreApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.windwail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringExploreApplication.class)
+public class SpringExploreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
